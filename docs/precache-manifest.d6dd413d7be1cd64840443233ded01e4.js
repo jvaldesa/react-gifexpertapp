ecd47a17ed515c692603f7d89239a9e9ed31a06d
Jvaldes <jvaldes_a@hotmail.com>
2022-01-16 12:06:23 -0600

    Primer comit

---
+++ docs/precache-manifest.d6dd413d7be1cd64840443233ded01e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "844a700442d1e318521b4ff9c941df22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcc7cbe8969c63d68977",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcc7cbe8969c63d68977",
+    "url": "/static/js/main.9fca5404.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
